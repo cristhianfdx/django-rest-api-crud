d2e16f3a66af6eb098d56c2f03517b7e51e7979b
Cristhian Forero <cristhian.forero.dominguez@gmail.com>
2020-01-25 23:01:22 -0500

    first commit

---
+++ crud/employee/models.py
@@ -0,0 +1,17 @@
+"""Employee Model."""
+
+from django.db import models
+
+
+class Employee(models.Model):
+    fullname = models.CharField(max_length=100)
+    document_number = models.CharField(max_length=20)
+    mobile_number = models.CharField(max_length=40)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'employee'
+
+    def __str__(self):
+        return self.fullname
